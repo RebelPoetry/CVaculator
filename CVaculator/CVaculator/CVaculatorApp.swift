752de2d38e52d974cd450eb96ca84a348835873d
Maks <max.g1312@gmail.com>
2022-06-26 23:35:26 +0300

    init

---
+++ CVaculator/CVaculator/CVaculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  CVaculatorApp.swift
+//  CVaculator
+//
+//  Created by Kazakh on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CVaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+            //dgfdg
+            //sd;.f
+        }//
+    }
+}
---
+++ CVaculator/CVaculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CVaculator
+//
+//  Created by Kazakh on 26.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+           // .padding()
+    }
+}////
+//ывавыаывавыавыа
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
